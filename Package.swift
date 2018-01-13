a99c862edc4cd11dd571d541b8cb49f6d3656b4f
Wojciech Baranski <savailonei@gmail.com>
2018-01-13 06:14:33 +0100

    commit

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Cboost",
-    pkgConfig: "libboost-all-dev",
+    //boost does not provide pkg-config file - it's a long requested feature
+    //need to use -Xlinker -L/usr/local/lib/
+    //pkgConfig: "libboost-all-dev",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
